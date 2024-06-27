69142614c4e023db7f54a83b848251ba7801c699
Wojciech Olech <wojciech.olech@mrq.com>
2024-06-27 19:03:35 +0200

    first commit

---
+++ src/main/java/com/nuxplanet/softpropsghrelease/SoftpropsGhReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.nuxplanet.softpropsghrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftpropsGhReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftpropsGhReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuxplanet/softpropsghrelease/SoftpropsGhReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuxplanet.softpropsghrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftpropsGhReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
